896b7820f87d6e9db1a71d2082a02fe25fdc03eb
luialbeto <araujo.script@gmail.com>
2020-08-13 17:09:18 -0300

    a saga capitulo 1

---
+++ src/components/Table.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Table extends Component {
+  render() {
+    const Table = () => <div>StarWars Datatable with Filters</div>;
+    return (
+      <div>
+      </div>
+    )
+  }
+}
