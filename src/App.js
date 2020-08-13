8b37915599717b5b304e0154cb58b57ab32c6846
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-08-13 16:13:22 -0300

    Carla Nakajuni - Iniciando o  Projeto StarWars

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Table from './components/Table';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Table />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
