787fbce4d0718f007dc57b6f39d2b85ded594c7a
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-08-13 18:33:16 -0300

    fetching working and list working

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,25 +1,91 @@
 import React, { Component } from 'react';
-
+import { handleAsyncFetch } from '../reducers';
+import { connect } from 'react-redux';
 class Table extends Component {
   constructor(props) {
     super(props);
-    this.state = {test: 'api'};
+    // this.state = { test: 'api' };
   }
 
   componentDidMount() {
-    this.setState({
-      test: 'hello',
-    });
+    const { handleAsync } = this.props;
+
+    handleAsync();
   }
 
+  // renderLoading() {
+  //   if(this.props.isfetching) return <div>Loading...</div>;
+  // }
+
   render() {
+    const { isfetching, data } = this.props;
     return (
-    <div>
-      StarWars Datatable with Filters <br></br>
-      {this.state.test}
-    </div>);
+      <div>
+        StarWars Datatable with Filters <br></br>
+        {isfetching && <h1>Loading...</h1>}
+
+        <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>rotation period</th>
+            <th>orbital period</th>
+            <th>diameter</th>
+            <th>climate</th>
+            <th>gravity</th>
+            <th>terrain</th>
+            <th>surface_water</th>
+            <th>population</th>
+            <th>films</th>
+            <th>created</th>
+          </tr>
+          </thead>
+          {!isfetching && data.map(planet => (
+            <tbody key={planet.name}>
+              <tr>
+                <td >{planet.name}</td>
+                <td>{planet.rotation_period}</td>
+                <td>{planet.orbital_period}</td>
+                <td>{planet.diameter}</td>
+                <td>{planet.climate}</td>
+                <td>{planet.gravity}</td>
+                <td>{planet.terrain}</td>
+                <td>{planet.surface_water}</td>
+                <td>{planet.population}</td>
+                <td>{planet.films}</td>
+                <td>{planet.created}</td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      </div>
+    );
   }
 }
 
-export default Table;
+const mapDispatchToProps = (dispatch) => ({
+  handleAsync: (e) => dispatch(handleAsyncFetch(e)),
+});
 
+const mapStateToProps = (state) => ({
+  isfetching: state.emptyReducer.isfetching,
+  data: state.emptyReducer.data,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Table);
+
+
+// climate: "arid"
+// created: "2014-12-09T13:50:49.641000Z"
+// diameter: "10465"
+// edited: "2014-12-20T20:58:18.411000Z"
+// films: (5) ["https://swapi-trybe.herokuapp.com/api/films/1/", "https://swapi-trybe.herokuapp.com/api/films/3/", "https://swapi-trybe.herokuapp.com/api/films/4/", "https://swapi-trybe.herokuapp.com/api/films/5/", "https://swapi-trybe.herokuapp.com/api/films/6/"]
+// gravity: "1 standard"
+// name: "Tatooine"
+// orbital_period: "304"
+// population: "200000"
+// residents: (10) ["https://swapi-trybe.herokuapp.com/api/people/1/", "https://swapi-trybe.herokuapp.com/api/people/2/", "https://swapi-trybe.herokuapp.com/api/people/4/", "https://swapi-trybe.herokuapp.com/api/people/6/", "https://swapi-trybe.herokuapp.com/api/people/7/", "https://swapi-trybe.herokuapp.com/api/people/8/", "https://swapi-trybe.herokuapp.com/api/people/9/", "https://swapi-trybe.herokuapp.com/api/people/11/", "https://swapi-trybe.herokuapp.com/api/people/43/", "https://swapi-trybe.herokuapp.com/api/people/62/"]
+// rotation_period: "23"
+// surface_water: "1"
+// terrain: "desert"
+// url: "https://swapi-trybe.herokuapp.com/api/planets/1/"
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,29 +2,59 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 const IS_REQUESTING = 'IS_REQUESTING';
+const ADD_DATA = 'ADD_DATA';
+const ADD_ERROR = 'ADD_ERROR';
 
 const isRequesting = () => ({
   type: IS_REQUESTING,
 });
 
+const dataActionCreator = (json) => ({
+  type: ADD_DATA,
+  data: json,
+});
+
+const errorActionCreator = (error) => ({
+  type: ADD_ERROR,
+  error,
+});
+
+
 const initialState = {
-  data: {},
+  data: [],
   isfetching: false,
 };
 
-function handleAsyncFetch() {
+const api = 'https://swapi-trybe.herokuapp.com/api/planets/';
+
+export function handleAsyncFetch(extension) {
   return (dispatch) => {
     dispatch(isRequesting());
 
-    return fetch()
-    .then
-    ;
+    return fetch(api)
+      .then((response) => response.json())
+      .then(
+        (json) => {
+          console.log(json.results);
+          return dispatch(dataActionCreator(json.results));
+        },
+        (error) => {
+          console.log('ta tudo errado: ' + error);
+          dispatch(errorActionCreator(error));
+        },
+      );
   };
 }
 
-
 function emptyReducer(state = initialState, action) {
   switch (action.type) {
+    case IS_REQUESTING:
+      return { ...state, isfetching: true}
+    case ADD_DATA:
+      return { ...state, data: action.data, isfetching: false };
+    case ADD_ERROR:
+      return { ...state, error: action.error, isfetching: false };
+
     default:
       return state;
   }
@@ -33,7 +63,6 @@ const rootReducer = combineReducers({ emptyReducer });
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
-
 
 // export function thunkCharacter(name) {
 //   return (dispatch) => {
@@ -45,3 +74,12 @@ export default store;
 //       );
 //   };
 // };
+
+// export const charAPI = () => (
+//   fetch(`${ISS_BASE_API}/iss-now.json`)
+//     .then((response) => (
+//       response
+//         .json()
+//         .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json)))
+//     ))
+// );
