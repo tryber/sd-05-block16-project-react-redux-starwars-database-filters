9bd371da32f0b41b2062665c554e645388b402e8
Rafael Assad <rafaassadeng@gmail.com>
2020-08-14 09:40:14 -0300

    HONESTO

--- src/services/planetsAPI.js
+++ src/services/planetsAPI.js
@@ -1,12 +1,10 @@
-const APIURL = 'https://swapi-trybe.herokuapp.com/api/planets'
+// usei o exercicio de redux de game of thrones como base para fazer essa requisição api
 
-const planetsAPI = (char) => (
-  fetch(`${APIURL}${char.split().join('+')}`)
-    .then((response) => (
-      response
-        .json()
-        .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json)))
-    ))
-);
+const APIURL = 'https://swapi-trybe.herokuapp.com/api/planets';
+
+const planetsAPI = (planet) =>
+  fetch(`${APIURL}${planet.split().join('+')}`).then((response) =>
+    response.json().then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json)))
+  );
 
 export default planetsAPI;
