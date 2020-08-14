54e09ff9f90fff313be051b7b50fd1cd07c67db2
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-08-14 16:26:01 -0300

    codeclimate

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,9 +5,6 @@ import PropTypes from 'prop-types';
 import Cabecalho from './Cabecalho';
 
 class Table extends Component {
-  constructor(props) {
-    super(props);
-  }
 
   filterByNumber(planets, filtro) {
     if (filtro.comparison === 'maior que') {
