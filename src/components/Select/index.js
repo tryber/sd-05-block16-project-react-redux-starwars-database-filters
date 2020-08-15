9be3b8b29ba6e50cfc5d4fb63334634da87ae8c4
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-08-15 15:57:43 -0300

    terceiro requisito feito

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { querySelector } from '../../actions';
-import SelectOption from '../SelectOptions';
-import SelectValues from '../SelectValues';
 
 class Select extends React.Component {
+
   constructor(props) {
     super(props);
 
-    this.state = {
+    this.state ={
       column: '',
       comparison: '',
       value: '',
-    };
+    }
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -20,36 +19,36 @@ class Select extends React.Component {
     const { name, value } = e.target;
     this.setState({
       [name]: value,
-    });
+    })
   }
-
+  
   render() {
-    const { column, comparison, value } = this.state;
-    const { filteredNumbers } = this.props;
+    const {column, comparison, value} = this.state;
+    const { filteredNumbers } = this.props
+    const options = [ 'selecione', 'population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water' ];
+    const optionsValues = [ 'selecione', 'maior que', 'menor que', 'igual a' ];
     return (
       <div>
-        <SelectOption />
-        <SelectValues />
-        <input
-          type='number'
-          data-testid='value-filter'
-          name='value'
-          onChange={this.handleChange}
-        />
-        <button
-          type='button'
-          data-testid='button-filter'
-          onClick={() => filteredNumbers({ column, comparison, value })}
-        >
-          Buscar
-        </button>
+        <select data-testid="column-filter" name='column' onChange={this.handleChange}>
+          {options.map((option) => 
+            <option key={option} value={option}>{option}</option>
+          )}
+        </select>
+        <select data-testid='comparison-filter' name='comparison' onChange={this.handleChange}>
+          {optionsValues.map((values) =>
+          <option key={values} value={values}>{values}</option>
+          )}
+        </select>
+        <input type="number" data-testid='value-filter' name='value' onChange={this.handleChange} />
+        <button type='button' data-testid='button-filter' onClick={() => filteredNumbers({column, comparison, value})}>Buscar</button>
       </div>
-    );
+    )
   }
 }
 
+
 const mapDispatchToProps = (dispatch) => ({
-  filteredNumbers: (e) => dispatch(querySelector(e)),
-});
+  filteredNumbers : (e) => dispatch(querySelector(e))
+})
 
 export default connect(null, mapDispatchToProps)(Select);
--- src/services/queryFilters.js
+++ src/services/queryFilters.js
@@ -8,21 +8,29 @@ export default function queryFilters(planet, textFilter, filters) {
   }
 
   filters.forEach((filter) => {
-    if (filter.comparison === 'maior que') { const greaterThan = planetsFiltered.filter((fil) =>
-        fil.name.toLowerCase().includes(textFilter.toLowerCase()));
+    if (filter.comparison === 'maior que') {
+      const greaterThan = planetsFiltered.filter((fil) =>
+        fil.name.toLowerCase().includes(textFilter.toLowerCase())
+      );
       planetsFiltered = greaterThan.filter(
-        (fil) => Number(fil[filter.column]) > Number(filter.value));
+        (fil) => Number(fil[filter.column]) > Number(filter.value),
+      );
     } else if (filter.comparison === 'menor que') {
       const lessThan = planetsFiltered.filter((fil) =>
-        fil.name.toLowerCase().includes(textFilter.toLowerCase()));
+        fil.name.toLowerCase().includes(textFilter.toLowerCase()),
+      );
       planetsFiltered = lessThan.filter(
-        (fil) => Number(fil[filter.column]) < Number(filter.value));
+        (fil) => Number(fil[filter.column]) < Number(filter.value),
+      );
     } else {
       const equalTo = planetsFiltered.filter((fil) =>
-        fil.name.toLowerCase().includes(textFilter.toLowerCase()));
+        fil.name.toLowerCase().includes(textFilter.toLowerCase()),
+      );
       planetsFiltered = equalTo.filter(
-        (fil) => Number(fil[filter.column]) === Number(filter.value));
+        (fil) => Number(fil[filter.column]) === Number(filter.value),
+      );
     }
   });
+  // console.log (planetsFiltered);
   return planetsFiltered;
 }
