c02e7a83648f44bfb21568dd86c8a423e4e8cfad
rfreitasbatista <rfreitasbatista@gmail.com>
2020-08-14 21:07:03 -0300

    Codeclimate 1

--- src/services/filtering.js
+++ src/services/filtering.js
@@ -6,30 +6,23 @@ export default function filtering(planets, NF, filters) {
     return textFilter;
   }
   let finalArr = planets;
-
   filters.forEach((filter) => {
     if(filter.comparison === 'maior que') {
       let biggerThan = finalArr.filter((data) =>
         data.name.toLowerCase().includes(NF.toLowerCase())
       );
-      finalArr = biggerThan.filter(
-        (data) => Number(data[filter.column]) > Number(filter.value)
-      );
+      finalArr = biggerThan.filter((data) => Number(data[filter.column]) > Number(filter.value));
     } else if(filter.comparison === 'menor que') {
         let lowerThan = finalArr.filter((data) =>
           data.name.toLowerCase().includes(NF.toLowerCase())
         );
-        finalArr = lowerThan.filter(
-          (data) => Number(data[filter.column]) < Number(filter.value)
-        );
+        finalArr = lowerThan.filter((data) => Number(data[filter.column]) < Number(filter.value));
     } else {
       let equalTo = finalArr.filter((data) =>
         data.name.toLowerCase().includes(NF.toLowerCase())
       );
-      finalArr = equalTo.filter(
-        (data) => Number(data[filter.column]) === Number(filter.value)
-      );
+      finalArr = equalTo.filter((data) => Number(data[filter.column]) === Number(filter.value));
     }
   });
-  return finalArr
+  return finalArr;
 }
