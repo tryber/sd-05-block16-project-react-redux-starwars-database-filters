175e085e48d29dff81f990a0e4d6855e3aa5b6ca
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-08-15 11:04:45 -0300

    added into store a way to visualize with redux devtools extension

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
-// caminho certo requerido para passar nos tests
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
+
+// [Honestidade acadêmica]
+// Código para integrar o Redux Devtools
+// achado pronto no repositório oficial da extension:
+// https://github.com/zalmoxisus/redux-devtools-extension
