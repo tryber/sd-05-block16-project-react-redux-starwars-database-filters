356ff411d9993e73a443c883d7b09618ff56672d
rfreitasbatista <rfreitasbatista@gmail.com>
2020-08-14 21:11:54 -0300

    Codeclimate 2

--- src/services/filtering.js
+++ src/services/filtering.js
@@ -1,27 +1,27 @@
 export default function filtering(planets, NF, filters) {
   if (filters.length < 1) {
     const textFilter = planets.filter((data) =>
-      data.name.toLowerCase().includes(NF.toLowerCase())
-    );
+      data.name.toLowerCase().includes(NF.toLowerCase()));
     return textFilter;
   }
   let finalArr = planets;
   filters.forEach((filter) => {
-    if(filter.comparison === 'maior que') {
-      let biggerThan = finalArr.filter((data) =>
-        data.name.toLowerCase().includes(NF.toLowerCase())
-      );
-      finalArr = biggerThan.filter((data) => Number(data[filter.column]) > Number(filter.value));
-    } else if(filter.comparison === 'menor que') {
-        let lowerThan = finalArr.filter((data) =>
-          data.name.toLowerCase().includes(NF.toLowerCase())
-        );
-        finalArr = lowerThan.filter((data) => Number(data[filter.column]) < Number(filter.value));
+    if (filter.comparison === 'maior que') {
+      const biggerThan = finalArr.filter((data) =>
+        data.name.toLowerCase().includes(NF.toLowerCase()));
+      finalArr = biggerThan.filter(
+        (data) => Number(data[filter.column]) > Number(filter.value));
+    } else if (filter.comparison === 'menor que') {
+      const lowerThan = finalArr.filter((data) =>
+        data.name.toLowerCase().includes(NF.toLowerCase()));
+      finalArr = lowerThan.filter(
+        (data) => Number(data[filter.column]) < Number(filter.value));
     } else {
-      let equalTo = finalArr.filter((data) =>
-        data.name.toLowerCase().includes(NF.toLowerCase())
+      const equalTo = finalArr.filter((data) =>
+        data.name.toLowerCase().includes(NF.toLowerCase()));
+      finalArr = equalTo.filter(
+        (data) => Number(data[filter.column]) === Number(filter.value)
       );
-      finalArr = equalTo.filter((data) => Number(data[filter.column]) === Number(filter.value));
     }
   });
   return finalArr;
