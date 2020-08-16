981bddb7d35fe14520a1d41a474d80a4109744b4
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-08-16 15:24:57 -0300

    Estrutura inicial de arquivos e requisição à API

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+<App />, document.getElementById('root'));
+</Provider>
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
