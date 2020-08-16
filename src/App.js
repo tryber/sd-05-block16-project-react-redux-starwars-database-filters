814208946f81ee70a11491326b7a6f756138f3d4
César Alves <alvescesar@hotmail.com>
2020-08-16 17:34:21 -0300

    Added PropTypes validation for the <App /> component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import './App.css';
 
@@ -24,6 +25,10 @@ class App extends Component {
 
 const mapDispatchToProps = {
   planetsData: fetchPlanets,
-}
+};
+
+App.propTypes = {
+  planetsData: PropTypes.func.isRequired,
+};
 
 export default connect(null, mapDispatchToProps)(App);
