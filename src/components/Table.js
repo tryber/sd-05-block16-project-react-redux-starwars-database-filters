2a2d71c1c0643ee7c5e0f8d7440cecafedcfbc8f
marinabrcls <barcelos.marina@gmail.com>
2020-08-17 16:29:05 -0300

    table test

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,33 +1,18 @@
 import React, { Component } from 'react';
+import TableHeader from './TableHeader';
+import TableBody from './TableBody';
 
 class Table extends Component {
-  constructor(props) {
-    super(props);
-    state = {}
-
-    render() {
-      return (
-        <div>
-          <h1>StarWars Datatable with Filters</h1>
-          <table>
-            <theader>
-              <tr>
-                <th>Test A</th>
-                <th>Test B</th>
-                <th>Test C</th>
-              </tr>
-            </theader>
-            <tbody>
-              <tr>
-                <td>Test 1</td>
-                <td>Test 2</td>
-                <td>Test 3</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div>
+        <p>Testando</p>
+        <table>
+          <TableHeader />
+          <TableBody />
+        </table>
+      </div>
+    );
   }
 }
 
---
+++ src/components/TableBody.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class TableBody extends Component {
+  render() {
+    return (
+      <tbody>
+        <tr>
+          <td>Test 1</td>
+          <td>Test 2</td>
+          <td>Test 3</td>
+        </tr>
+      </tbody>
+    );
+  }
+}
+
+export default TableBody;
