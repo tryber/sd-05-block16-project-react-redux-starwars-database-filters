d03184a07d8d13d820d01047d733700baeed9255
marinabrcls <barcelos.marina@gmail.com>
2020-08-17 18:19:22 -0300

    table header with 13 columns

--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -6,19 +6,18 @@ class TableHeader extends Component {
       <thead>
         <tr>
           <th>Name</th>
-          <th>Rotation period</th>
-          <th>Orbital period</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
           <th>Diameter</th>
           <th>Climate</th>
           <th>Gravity</th>
           <th>Terrain</th>
-          <th>Surface water</th>
+          <th>Surface Water</th>
           <th>Population</th>
-          <th>Residents</th>
           <th>Films</th>
           <th>Created</th>
           <th>Edited</th>
-          <th>Url</th>
+          <th>URL</th>
         </tr>
       </thead>
     );
