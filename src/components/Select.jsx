4561b82008e5d645b1f299915d3b5197fbf8f134
Lucas Costa Ventura <lucascv333@gmail.com>
2020-08-17 20:44:14 -0300

    requisito 3

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -4,11 +4,39 @@ import PropTypes from 'prop-types';
 import { numericFilter } from '../actions';
 
 class Select extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      column: '',
+      comparison: '',
+      value: '',
+    };
+    this.changeState = this.changeState.bind(this);
+    this.changeFilters = this.changeFilters.bind(this);
+  }
+
+  changeState(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  // Repositório do Anderson Godoy melhorou minha aplicação devido aos campos não selecionados
+  changeFilters() {
+    const { column, comparison, value } = this.state;
     const { filteredNumbers } = this.props;
+    if (column === 'selecione' || comparison === 'selecione' || value === '') {
+      alert('Todos os campos são Obrigatórios');
+    } else {
+      filteredNumbers({ column, comparison, value });
+    }
+  }
+
+  render() {
     return (
       <div>
-        <select data-testid="column-filter" name="column">
+        <select data-testid="column-filter" name="column" onChange={this.changeState}>
           <option value="selecione">selecione</option>
           <option value="population">population</option>
           <option value="orbital_period">orbital_period</option>
@@ -16,17 +44,17 @@ class Select extends React.Component {
           <option value="rotation_period">rotation_period</option>
           <option value="surface_water">surface_water</option>
         </select>
-        <select data-testid="comparison-filter" name="comparison">
+        <select data-testid="comparison-filter" name="comparison" onChange={this.changeState}>
           <option value="selecione">selecione</option>
           <option value="maior que">maior que</option>
           <option value="menor que">menor que</option>
           <option value="igual a">igual a</option>
         </select>
-        <input type="number" data-testid="value-filter" />
+        <input type="number" data-testid="value-filter" name="value" onChange={this.changeState} />
         <button
           type="button"
           data-testid="button-filter"
-          onClick={(event) => filteredNumbers(event.target.value)}
+          onClick={() => this.changeFilters()}
         >
           Filtrar
         </button>
