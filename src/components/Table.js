67694c3c9e0bc143e52b3d3323640d14df8f9309
marinabrcls <barcelos.marina@gmail.com>
2020-08-16 22:05:44 -0300

    esqueleto da tabela

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Table = () => <div>StarWars Datatable with Filters</div>;
+class Table extends Component {
+  constructor(props) {
+    super(props);
+    state = {}
+
+    render() {
+      return (
+        <div>
+          <h1>StarWars Datatable with Filters</h1>
+          <table>
+            <tbody>
+              <tr>
+                <td>Test 1</td>
+                <td>Test 2</td>
+                <td>Test 3</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      );
+    }
+  }
+}
 
 export default Table;
