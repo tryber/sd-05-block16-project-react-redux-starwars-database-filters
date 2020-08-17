0242c2f3de29ef9d405d3c9c53e77e59417858b3
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-08-17 18:18:52 -0300

    Completando as informações da Tabela com os dados que foram 'fetch' da API

--- src/components/TableInfo.js
+++ src/components/TableInfo.js
@@ -1,17 +1,41 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 // tabela que receberá o corpo da função
 
 class TableInfo extends React.Component {
   render() {
+    const { data } = this.props;
     return (
-      <tbody> 
-        <tr>
-          <td></td>
-          <td></td>
-        </tr> 
-      </tbody>
+      data.map((planet) => (
+        <tbody key={planet.name}> 
+          <td>{planet.name}</td>
+          <td>{planet.rotation_period}</td>
+          <td>{planet.orbital_period}</td>
+          <td>{planet.diameter}</td>
+          <td>{planet.climate}</td>
+          <td>{planet.gravity}</td>
+          <td>{planet.terrain}</td>
+          <td>{planet.surface_water}</td>
+          <td>{planet.population}</td>
+          <td>{planet.films}</td>
+          <td>{planet.created}</td>
+          <td>{planet.edited}</td>
+          <td>{planet.url}</td>
+        </tbody>
+      ))
     );
   }
 }
-export default TableInfo;
+
+const mapDispatchToProps = (state) => ({
+  data: state.planetReducer.data,
+});
+
+
+export default connect(mapDispatchToProps)(TableInfo);
+
+TableInfo.propTypes = {
+  data: propTypes.arrayOf(propTypes.object).isRequired,
+};
