f4961ec6d77455c1992b53e838e18a0e4fb170fd
RenataNovais <rnovaiscs@gmail.com>
2020-08-17 17:35:37 -0300

    primeiro requisito

--- src/components/Table.js
+++ src/components/Table.js
@@ -15,6 +15,25 @@ class Table extends React.Component {
                 key === 'residents' ? false : <th>{key}</th>))}
             </tr>
           </thead>
+          <tbody>
+            {data.map((result) => (
+              <tr>
+                <td>{result.name}</td>
+                <td>{result.rotation_period}</td>
+                <td>{result.orbital_period}</td>
+                <td>{result.diameter}</td>
+                <td>{result.climate}</td>
+                <td>{result.gravity}</td>
+                <td>{result.terrain}</td>
+                <td>{result.surface_water}</td>
+                <td>{result.population}</td>
+                <td>{result.films}</td>
+                <td>{result.created}</td>
+                <td>{result.edited}</td>
+                <td>{result.url}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     );
