9f6c1f601edde7ce5ab12e442743ed589dd7a34d
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-08-17 01:28:53 -0300

    Implementação cabeçãlho da Tabela

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from './store';
 import * as serviceWorker from './serviceWorker';
+
+import { Provider } from 'react-redux';
+
+import store from './store';
+
 
 ReactDOM.render(
   <Provider store={store}>
