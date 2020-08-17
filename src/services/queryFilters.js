a98e8b84fc91e663992be6f3d1c1a4b2d71b8726
Lucas Costa Ventura <lucascv333@gmail.com>
2020-08-17 20:18:19 -0300

    code climate

--- src/services/queryFilters.js
+++ src/services/queryFilters.js
@@ -15,17 +15,20 @@ export default function queryFilters(data, textFilter, filters) {
       const greaterThan = planetsFiltered.filter((fil) =>
         fil.name.toLowerCase().includes(textFilter.toLowerCase()));
       planetsFiltered = greaterThan.filter(
-        (fil) => Number(fil[filter.column]) > Number(filter.value));
+        (fil) => Number(fil[filter.column]) > Number(filter.value),
+      );
     } else if (filter.comparison === 'menor que') {
       const lessThan = planetsFiltered.filter((fil) =>
         fil.name.toLowerCase().includes(textFilter.toLowerCase()));
       planetsFiltered = lessThan.filter(
-        (fil) => Number(fil[filter.column]) < Number(filter.value));
+        (fil) => Number(fil[filter.column]) < Number(filter.value),
+      );
     } else {
       const equalTo = planetsFiltered.filter((fil) =>
         fil.name.toLowerCase().includes(textFilter.toLowerCase()));
       planetsFiltered = equalTo.filter(
-        (fil) => Number(fil[filter.column]) === Number(filter.value));
+        (fil) => Number(fil[filter.column]) === Number(filter.value),
+      );
     }
   });
   return planetsFiltered;
