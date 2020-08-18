710654edf1218335dfde9db2941243539b78116f
RenataNovais <rnovaiscs@gmail.com>
2020-08-18 13:53:26 -0300

    resolvido pelo PR

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -4,56 +4,61 @@ import propTypes from 'prop-types';
 import { filterName, filterByNumeric, replaceFilters } from '../actions';
 import Select from './Select';
 
+function handleConst(selectedFilters) {
+  const dafaultColumnOpt = ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
+  const comparisonOpt = ['maior que', 'menor que', 'igual a'];
+  let columnOpt = dafaultColumnOpt;
+  selectedFilters.forEach((filter) => {
+    columnOpt = columnOpt.filter((opt) => opt !== filter.column);
+    return columnOpt;
+  });
+  const allV = {};
+  return {
+    dafaultColumnOpt,
+    comparisonOpt,
+    columnOpt,
+    allV,
+  };
+}
+
 class FilterBar extends Component {
   constructor(props) {
     super(props);
-    this.handleConst = this.handleConst.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
-  handleConst (selectedFilters) {
-    const dafaultColumnOpt = ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
-    const comparisonOpt = ['maior que', 'menor que', 'igual a'];
-    let columnOpt = dafaultColumnOpt;
-    selectedFilters.forEach((filter) => {
-      columnOpt = columnOpt.filter((opt) => opt !== filter.column)
-      return columnOpt;
-    });
-    const allValues = {};
-    return {
-      dafaultColumnOpt,
-      comparisonOpt,
-      columnOpt,
-      allValues,
-    }
-  }
 
-  handleClick (allValues) {
-    if (allValues.column && allValues.comparison && allValues.num)
-    return this.props.filterByNum(allValues.column, allValues.comparison, allValues.num);
+  handleClick(allV) {
+    if (allV.column && allV.comparison && allV.num) {
+      return this.props.filterByNum(allV.column, allV.comparison, allV.num);
+    }
+    return false;
   }
 
   render() {
     const { nameInput, selectedFilters, replaceAll } = this.props;
-    const { comparisonOpt, columnOpt, allValues } = this.handleConst(selectedFilters);
+    const { comparisonOpt, columnOpt, allV } = handleConst(selectedFilters);
     return (
       <div>
-        <input data-testid="name-filter" type="text" name="name-filter" onChange={(e) => nameInput(e.target.value)}/>
+        <input data-testid="name-filter" type="text" onChange={(e) => nameInput(e.target.value)} />
         <Select
           dataTestId="column-filter" defaultOpt="Coluna" arrayOpt={columnOpt}
-          onChange={(e) => { allValues.column = e.target.value; }}
+          onChange={(e) => { allV.column = e.target.value; }}
         />
         <Select
           dataTestId="comparison-filter" defaultOpt="Comparacao" arrayOpt={comparisonOpt}
-          onChange={(e) => { allValues.comparison = e.target.value; }}
+          onChange={(e) => { allV.comparison = e.target.value; }}
         />
-        <input required type="number" data-testid="value-filter" onChange={(e) => { allValues.num = e.target.value; }} />
-        <button data-testid="button-filter" type="submit" onClick={() => this.handleClick(allValues)}>
+        <input type="number" data-testid="value-filter" onChange={(e) => { allV.num = e.target.value; }} />
+        <button data-testid="button-filter" type="submit" onClick={() => this.handleClick(allV)}>
           Filtrar
         </button>
         {selectedFilters.map((filter) => (
           <div data-testid="filter">
-            <label>{`${filter.column} ${filter.comparison} ${filter.value}`}</label>
-            <button onClick={() => replaceAll(selectedFilters.filter((d) => d.column !== filter.column))}>
+            <span>{`${filter.column} ${filter.comparison} ${filter.value}`}</span>
+            <button
+              type="button"
+              onClick={() => replaceAll(selectedFilters.filter((d) => d.column !== filter.column))}
+            >
               X
             </button>
           </div>
