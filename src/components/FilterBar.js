6238c4c2b00e25f40be66111f7b23f743bf6ec5b
RenataNovais <rnovaiscs@gmail.com>
2020-08-18 12:18:39 -0300

    ajeitando codeclimate no filterbar

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -2,50 +2,57 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import propTypes from 'prop-types';
 import { filterName, filterByNumeric, replaceFilters } from '../actions';
+import Select from './Select';
 
 class FilterBar extends Component {
-  render() {
-    const { nameInput, filterByNum, selectedFilters, replaceAll } = this.props;
+  handleConst = (selectedFilters) => {
     const dafaultColumnOpt = ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
     const comparisonOpt = ['maior que', 'menor que', 'igual a'];
     let columnOpt = dafaultColumnOpt;
-    selectedFilters.forEach((filter) => columnOpt = columnOpt.filter((opt) => opt !== filter.column));
+    selectedFilters.forEach((filter) => {
+      columnOpt = columnOpt.filter((opt) => opt !== filter.column)
+      return columnOpt;
+    });
     const allValues = {};
+    return {
+      dafaultColumnOpt,
+      comparisonOpt,
+      columnOpt,
+      allValues,
+    }
+  }
+
+  handleClick = (allValues) => {
+    if (allValues.column && allValues.comparison && allValues.num)
+    return this.props.filterByNum(allValues.column, allValues.comparison, allValues.num);
+  }
+
+  render() {
+    const { nameInput, selectedFilters, replaceAll } = this.props;
+    const { comparisonOpt, columnOpt, allValues } = this.handleConst(selectedFilters);
     return (
       <div>
-        <input
-          data-testid="name-filter"
-          type="text"
-          name="name-filter"
-          onChange={(e) => nameInput(e.target.value)}
+        <input data-testid="name-filter" type="text" name="name-filter" onChange={(e) => nameInput(e.target.value)}/>
+        <Select
+          dataTestId="column-filter" defaultOpt="Coluna" arrayOpt={columnOpt}
+          onChange={(e) => { allValues.column = e.target.value; }}
         />
-        <div>
-          <select required data-testid="column-filter" onChange={(e) => { allValues.column = e.target.value; }}>
-            <option disabled selected>Coluna</option>
-            {columnOpt.map((item) => <option value={item}>{item}</option>)}
-          </select>
-          <select required data-testid="comparison-filter" onChange={(e) => { allValues.comparison = e.target.value; }}>
-            <option disabled selected>Comparacao</option>
-            {comparisonOpt.map((item) => <option value={item}>{item}</option>)}
-          </select>
-          <div>
-            <input required type="number" data-testid="value-filter" onChange={(e) => { allValues.num = e.target.value; }} />
-          </div>
-        </div>
-        <button data-testid="button-filter" type="submit" onClick={() => 
-          (allValues.column && allValues.comparison && allValues.num) ?
-          filterByNum(allValues.column, allValues.comparison, allValues.num) : ''}
-        >
+        <Select
+          dataTestId="comparison-filter" defaultOpt="Comparacao" arrayOpt={comparisonOpt}
+          onChange={(e) => { allValues.comparison = e.target.value; }}
+        />
+        <input required type="number" data-testid="value-filter" onChange={(e) => { allValues.num = e.target.value; }} />
+        <button data-testid="button-filter" type="submit" onClick={() => this.handleClick(allValues)}>
           Filtrar
         </button>
-        <div>
-          {selectedFilters.map((filter) => (
-            <div data-testid="filter">
-              <label>{`${filter.column} ${filter.comparison} ${filter.value}`}</label>
-              <button type="button" onClick={() => replaceAll(selectedFilters.filter((d) => d.column !== filter.column))}>X</button>
-            </div>
-          ))}
-        </div>
+        {selectedFilters.map((filter) => (
+          <div data-testid="filter">
+            <label>{`${filter.column} ${filter.comparison} ${filter.value}`}</label>
+            <button onClick={() => replaceAll(selectedFilters.filter((d) => d.column !== filter.column))}>
+              X
+            </button>
+          </div>
+        ))}
       </div>
     );
   }
