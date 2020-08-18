878df2473cb07f0816970efcd2b7e916a6ad1084
RenataNovais <rnovaiscs@gmail.com>
2020-08-18 13:11:38 -0300

    voltei atras

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -4,52 +4,58 @@ import propTypes from 'prop-types';
 import { filterName, filterByNumeric, replaceFilters } from '../actions';
 import Select from './Select';
 
-function FilterBar(props) {
-  const { nameInput, selectedFilters, replaceAll } = props;
-  const dafaultColumnOpt = ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
-  const comparisonOpt = ['maior que', 'menor que', 'igual a'];
-  const allV = {};
-  let columnOpt = dafaultColumnOpt;
-  selectedFilters.forEach((filter) => {
-    columnOpt = columnOpt.filter((opt) => opt !== filter.column);
-    return columnOpt;
-  });
+class FilterBar extends Component {
+  handleConst = (selectedFilters) => {
+    const dafaultColumnOpt = ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
+    const comparisonOpt = ['maior que', 'menor que', 'igual a'];
+    let columnOpt = dafaultColumnOpt;
+    selectedFilters.forEach((filter) => {
+      columnOpt = columnOpt.filter((opt) => opt !== filter.column)
+      return columnOpt;
+    });
+    const allValues = {};
+    return {
+      dafaultColumnOpt,
+      comparisonOpt,
+      columnOpt,
+      allValues,
+    }
+  }
 
-  return (
-    <div>
-      <input data-testid="name-filter" type="text" name="name-filter" onChange={(e) => nameInput(e.target.value)} />
-      <Select
-        dataTestId="column-filter"
-        defaultOpt="Coluna"
-        arrayOpt={columnOpt}
-        onChange={(e) => { allV.column = e.target.value; }}
-      />
-      <Select
-        dataTestId="comparison-filter"
-        defaultOpt="Comparacao"
-        arrayOpt={comparisonOpt}
-        onChange={(e) => { allV.comparison = e.target.value; }}
-      />
-      <input required type="number" data-testid="value-filter" onChange={(e) => { allV.num = e.target.value; }} />
-      <button
-        data-testid="button-filter"
-        type="submit"
-        onClick={() => {
-          if (allV.column && allV.comparison && allV.num) props.filterByNum(allV.column, allV.comparison, allV.num);
-        }}
-      >
-        Filtrar
-      </button>
-      {selectedFilters.map((filter) => (
-        <div data-testid="filter">
-          <label>{`${filter.column} ${filter.comparison} ${filter.value}`}</label>
-          <button type="button" onClick={() => replaceAll(selectedFilters.filter((d) => d.column !== filter.column))}>
-            X
-          </button>
-        </div>
-      ))}
-    </div>
-  );
+  handleClick = (allValues) => {
+    if (allValues.column && allValues.comparison && allValues.num)
+    return this.props.filterByNum(allValues.column, allValues.comparison, allValues.num);
+  }
+
+  render() {
+    const { nameInput, selectedFilters, replaceAll } = this.props;
+    const { comparisonOpt, columnOpt, allValues } = this.handleConst(selectedFilters);
+    return (
+      <div>
+        <input data-testid="name-filter" type="text" name="name-filter" onChange={(e) => nameInput(e.target.value)}/>
+        <Select
+          dataTestId="column-filter" defaultOpt="Coluna" arrayOpt={columnOpt}
+          onChange={(e) => { allValues.column = e.target.value; }}
+        />
+        <Select
+          dataTestId="comparison-filter" defaultOpt="Comparacao" arrayOpt={comparisonOpt}
+          onChange={(e) => { allValues.comparison = e.target.value; }}
+        />
+        <input required type="number" data-testid="value-filter" onChange={(e) => { allValues.num = e.target.value; }} />
+        <button data-testid="button-filter" type="submit" onClick={() => this.handleClick(allValues)}>
+          Filtrar
+        </button>
+        {selectedFilters.map((filter) => (
+          <div data-testid="filter">
+            <label>{`${filter.column} ${filter.comparison} ${filter.value}`}</label>
+            <button onClick={() => replaceAll(selectedFilters.filter((d) => d.column !== filter.column))}>
+              X
+            </button>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
