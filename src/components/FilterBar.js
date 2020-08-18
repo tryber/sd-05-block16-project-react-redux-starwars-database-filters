275bd510596f165748045c329571af76d474cac5
RenataNovais <rnovaiscs@gmail.com>
2020-08-17 21:42:24 -0300

    cc

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -5,14 +5,14 @@ import { filterName } from '../actions';
 
 class FilterBar extends Component {
   render() {
-    const { filterName } = this.props;
+    const { nameInput } = this.props;
     return (
       <div>
         <input
           data-testid="name-filter"
           type="text"
           name="name-filter"
-          onChange={(e) => filterName(e.target.value)}
+          onChange={(e) => nameInput(e.target.value)}
         />
       </div>
     );
@@ -20,11 +20,11 @@ class FilterBar extends Component {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  filterName: (inputText) => dispatch(filterName(inputText)),
+  nameInput: (inputText) => dispatch(filterName(inputText)),
 });
 
 export default connect(null, mapDispatchToProps)(FilterBar);
 
 FilterBar.propTypes = {
-  filterName: propTypes.func.isRequired,
+  nameInput: propTypes.func.isRequired,
 };
