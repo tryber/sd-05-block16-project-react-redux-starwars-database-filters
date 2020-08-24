74cb82bd1040c967dc34f4614c0d138f07e245f0
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-08-24 00:57:40 -0300

    CC again :(

--- src/App.js
+++ src/App.js
@@ -3,28 +3,28 @@ import './App.css';
 import Table from './components/Table';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       search: '',
-    }
+    };
     this.searchPlanet = this.searchPlanet.bind(this);
   }
 
   searchPlanet(e) {
     this.setState({
       search: e.target.value,
-    })
+    });
   }
 
   render() {
-  return (
-    <div>
-      Procurar:
-      <input onChange={(e) => this.searchPlanet(e)} type="text" />
-      <Table search={this.state.search}/>
-    </div>
-  );
+    return (
+      <div>
+        Procurar:
+        <input onChange={(e) => this.searchPlanet(e)} type="text" />
+        <Table search={this.state.search} />
+      </div>
+    );
   }
 }
 
