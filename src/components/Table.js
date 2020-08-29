30ce5e6eecb15171a9ab12ff6cb4ddaf6b66f7b3
Sidnei Ramos <sidneiramos02@gmail.com>
2020-08-29 19:53:27 -0300

    Corrigindo

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,19 +3,3 @@ import React from 'react';
 const Table = () => <div>StarWars Datatable with Filters</div>;
 
 export default Table;
-import React from 'react';
-import './App.css';
-
-import Table from './components/Table';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Table />
-      </header>
-    </div>
-  );
-}
-
-export default App;
