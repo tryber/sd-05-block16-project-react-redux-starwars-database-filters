2ca0faf2dd6e5380da8561e3dd51ace48abf6cf9
Marylange <marylangess@gmail.com>
2020-08-31 10:17:07 -0300

    CC

--- src/components/Table.js
+++ src/components/Table.js
@@ -93,7 +93,8 @@ function filterByNumber(arrayPlanets, filterByNumericValues) {
   return arrayPlanets;
 }
 
-/* A function filterByNumber foi retirado do código da minha colega de turma Nat Macedo e adpatado para o meu código*/
+/* A function filterByNumber foi retirado do código
+da minha colega de turma Nat Macedo e adpatado para o meu código*/
 
 const filtraPlanetas = (planetas, filtroDeTexto, filterByNumericValues) => {
   console.log('planetas', planetas);
