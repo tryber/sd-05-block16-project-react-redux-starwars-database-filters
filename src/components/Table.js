2b3d8234d816145624e8238ec6c03f2ab86d34a9
ederusena <eder.sena@live.com>
2020-09-01 19:24:40 -0300

    Reader

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-
 import { connect } from 'react-redux';
-
+import PropTypes from 'prop-types';
 import { fetchActionPlanets } from '../redux/actions';
 
 class Table extends Component {
@@ -16,28 +15,46 @@ class Table extends Component {
   }
 
   render() {
+    const { planets } = this.props.planetReducer;
+    console.log('reducer', this.props.planetReducer)
 
-    const { planetReducer } = this.props;
-    const { isFetching, planets } = planetReducer;
-    console.log("Planets", planets);
+    let planetas = [];
+    // GODOY
+    if (planets.length > 0) planetas = Object.keys(planets[0]);
 
+    console.log('planetasaasa', planetas)
     return (
-      <div>
-        <h1>{isFetching}</h1>
-      </div>
+      <table>
+        <thead>
+          <tr>
+            {planetas.map((header) => (
+              <th key={header}>{header}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {planets.map((info) => (
+            <tr key={(((1+Math.random())*0x10000)|0).toString(16).substring(1)}>
+              {planetas.map((data) => (
+                <td key={(((1+Math.random())*0x10000)|0).toString(16).substring(1)}>{info[data]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
   planetReducer: {
-    isFetching: state.planetReducer.isFetching,
+    loading: state.planetReducer.isFetching,
     planets: state.planetReducer.planets,
   },
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  getPlanets: () => dispatch(fetchActionPlanets()),
+  getPlanets: (e) => dispatch(fetchActionPlanets(e)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Table);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const PLANET_API = 'https://swapi-trybe.herokuapp.com/api/planets/';
+const PLANET_API = 'https://swapi-trybe.herokuapp.com/api/planets/?format=json';
 
 export const PLANETS_REQUEST_FAILURE = 'PLANETS_REQUEST_FAILURE';
 export const PLANETS_REQUEST_SUCCESS = 'PLANETS_REQUEST_SUCCESS';
@@ -24,10 +24,10 @@ export function fetchActionPlanets() {
   return (dispatch) => {
     dispatch(requestPlanets());
 
-    return axios.get(PLANET_API)
-      .then(
-        (response) => dispatch(requestPlanetsSuccess(response.data)),
+    return fetch(PLANET_API)
+      .then((response) => response.json()
+      .then((data) => dispatch(requestPlanetsSuccess(data.results)),
         (erro) => dispatch(requestPlanetsFailure(erro)),
-      )
-  }
+      ));
+  };
 }
