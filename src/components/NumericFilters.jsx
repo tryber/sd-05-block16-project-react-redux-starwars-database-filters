1ccdb5c52d82cb77c10c2bef2931d9506e824235
Mau <mauricio.sdjr@gmail.com>
2020-09-01 16:59:30 -0300

    fix filters bug

--- src/components/NumericFilters.jsx
+++ src/components/NumericFilters.jsx
@@ -37,23 +37,20 @@ class NumericFilters extends React.Component {
   render() {
     const options = ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
     const options2 = ['menor que', 'maior que', 'igual'];
-    const {
-      handleChange, handleClick, compChange, colChange,
-    } = this.props;
 
     return (
       <div>
-        <select data-testid="column-filter" placeholder="Selecione" onChange={colChange}>
-          <option value="" disabled selected>Selecione</option>
+        <select data-testid="column-filter" placeholder="Selecione" onChange={this.colChange}>
+          {/* <option value="" disabled selected>Selecione</option> */}
           {/*  https://stackoverflow.com/questions/5805059/how-do-i-make-a-placeholder-for-a-select-box */}
           {options.map((opcao) => (<option>{opcao}</option>))}
         </select>
-        <select data-testid="comparison-filter" onChange={compChange}>
-          <option value="" disabled selected>Selecione</option>
+        <select data-testid="comparision-filter" onChange={this.compChange}>
+          {/* <option value="" disabled selected>Selecione</option> */}
           {options2.map((opcao) => (<option>{opcao}</option>))}
         </select>
-        <input data-testid="value-filter" type="number" onChange={handleChange} />
-        <button type="button" data-testid="button-filter" onClick={handleClick}>Filtrar</button>
+        <input data-testid="value-filter" type="number" onChange={this.valueChange} />
+        <button type="button" data-testid="button-filter" onClick={this.handleClick}>Filtrar</button>
       </div>
     );
   }
@@ -68,8 +65,4 @@ export default connect(null, mapDispatchToProps)(NumericFilters);
 
 NumericFilters.propTypes = {
   filterNValues: PropTypes.func.isRequired,
-  handleChange: PropTypes.func.isRequired,
-  handleClick: PropTypes.func.isRequired,
-  compChange: PropTypes.func.isRequired,
-  colChange: PropTypes.func.isRequired,
 };
