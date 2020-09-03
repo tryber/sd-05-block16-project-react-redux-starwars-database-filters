3d145da5b3e8fcd043ebdc514be97c04622bc409
ederusena <eder.sena@live.com>
2020-09-03 17:21:09 -0300

    Requirement 2

--- src/components/FilterPlanet/index.js
+++ src/components/FilterPlanet/index.js
@@ -4,11 +4,11 @@ import Proptypes from 'prop-types';
 import { filterByName } from '../../actions';
 
 const FilterPlanet = (props) => {
-    <input
-      data-testid="name-filter"
-      type="text"
-      onChange={(e) => props.seekPlanet(e.target.value)}
-    />
+  <input
+    data-testid="name-filter"
+    type="text"
+    onChange={(e) => props.seekPlanet(e.target.value)}
+  />;
 };
 
 const mapDispatchToProps = (dispatch) => ({
