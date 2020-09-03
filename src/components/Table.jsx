52d72ba6f5fd3e968695f703d171ff113ab44549
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-09-03 16:38:16 -0300

    code climate maldito

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import Header from './Header';
 import Body from './Body';
 
-const Table = () => {
-  return (
-    <div>
-      <h1>StarWars Datatable with Filters</h1>
-      <table>
-        <Header />
-        <Body />
-      </table>
-    </div>
-  );
-};
+const Table = () => (
+  <div>
+    <h1>StarWars Datatable with Filters</h1>
+    <table>
+      <Header />
+      <Body />
+    </table>
+  </div>
+);
 
 export default Table;
