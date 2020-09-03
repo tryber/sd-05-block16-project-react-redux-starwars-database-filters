e8b85c791b1f42789d34b75e0edb94d7a4de5409
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-09-03 06:51:13 -0300

    outra tentativa de resolver a linha

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,8 @@ import filters from './Filters';
 import temporaryFilter from './TempFilterReducer';
 import data from './Data';
 
+// chatisse de linha no final que não resolve
+
 const rootReducer = combineReducers({
   filters,
   temporaryFilter,
