51ebc1964532a0804d5bec9bbef353ea9cec343e
hleoc <informig@gmail.com>
2020-09-04 19:52:49 -0300

    Itens 3 e 4 - corrigindo outros erros do codeclimateeee

--- src/components/BotaoFiltrar.js
+++ src/components/BotaoFiltrar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import Proptypes from 'prop-types';
+import Proptypes from 'prop-types';
 import filtroCompleto from '../actions/filtroCompleto';
 
 class BotaoFiltrar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-  handleClick = () => {
+  handleClick() {
     /* console.log(this.props) */
     const { coluna, comparacao, valor, filter } = this.props;
     filter({ column: coluna, comparison: comparacao, value: valor });
@@ -36,11 +40,11 @@ const mapDispatchToProps = (dispatch) => ({
   filter: (objetao) => dispatch(filtroCompleto(objetao)),
 });
 
-/* BotaoFiltrar.propTypes = {
+BotaoFiltrar.propTypes = {
   filter: Proptypes.func.isRequired,
   valor: Proptypes.arrayOf(Proptypes.object).isRequired,
   comparacao: Proptypes.arrayOf(Proptypes.object).isRequired,
   coluna: Proptypes.arrayOf(Proptypes.object).isRequired,
-}; */
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(BotaoFiltrar);
