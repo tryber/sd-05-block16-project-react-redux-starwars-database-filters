c0b2b2841c9c122724d60bee893e9e8c92297598
hleoc <informig@gmail.com>
2020-09-04 18:41:08 -0300

    Itens 3 e 4 - corrigindo mais erros do codeclimate

--- src/reducers/select.js
+++ src/reducers/select.js
@@ -1,4 +1,4 @@
-const INITIAL_STATE = {  
+const INITIAL_STATE = {
   column: '',
   comparison: '',
   value: '',
@@ -11,22 +11,22 @@ const select = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         filterByName: { name: action.payload },
-      }
+      };
     case 'SELECIONAR_TITULO':
       return {
         ...state,
         column: action.coluna,
-      }
+      };
     case 'SELECIONAR_COMPARACAO':
       return {
         ...state,
         comparison: action.comparacao,
-      }
+      };
     case 'FILTRO_POR_NUMERO':
       return {
         ...state,
         value: action.valor,
-      }   
+      };
     default:
       return state;
   }
