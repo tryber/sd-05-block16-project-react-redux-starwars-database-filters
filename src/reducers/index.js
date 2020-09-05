e08cfaf447249cc5f58e15367c61a7e057956e2b
Thiago <asm.thiago@gmail.com>
2020-09-04 21:49:49 -0300

    CC

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import apiReducer from './apiReducer';
-import filters from './filtersReducers'
+import filters from './filtersReducers';
 
 const rootReducer = combineReducers({
   apiReducer,
