7083a60348dea43ad6dae45df4cea636999638bc
Larissa Palombo <larissampalombo@gmail.com>
2020-09-30 18:50:12 -0300

    cc

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // Plantão do hamaji - quinta 5-9r
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import endpoint from './service/api';
-import { Provider } from 'react-redux';
 import store from './store';
 
 endpoint();
@@ -14,7 +14,7 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
