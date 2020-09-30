786f7cf41e1adde9da91ccad8069dc9df0eadcf1
SamuelPPlab <samuelpp.s@hotmail.com>
2020-09-30 19:51:55 -0300

    me deixa em paz code climate

--- src/components/RadioInput.jsx
+++ src/components/RadioInput.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 
-const RadioInput = ({ options, onChange, orderSelected, datatestid }) => {
-  return (
-    options.map((tipo) => (
-      <label
-        key={tipo}
-        htmlFor={tipo}
-      >
-        <input
-          data-testid={datatestid}
-          type="radio"
-          tipo={tipo}
-          value={tipo}
-          checked={orderSelected === tipo}
-          onChange={onChange}
-          id={tipo}
-        /> {tipo}
-      </label>
-    )
-  )
-  );
-};
+const RadioInput = ({ options, onChange, orderSelected, datatestid }) => (
+options.map((tipo) => (
+  <label
+    key={tipo}
+    htmlFor={tipo}
+  >
+    <input
+      data-testid={datatestid}
+      type="radio"
+      tipo={tipo}
+      value={tipo}
+      checked={orderSelected === tipo}
+      onChange={onChange}
+      id={tipo}
+    /> {tipo}
+  </label>
+))
+);
+
 
 export default RadioInput;
