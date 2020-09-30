a839334f6bf8b3596aba264c4629a5e1d5928e58
SamuelPPlab <samuelpp.s@hotmail.com>
2020-09-30 18:26:00 -0300

    cc quase pronto2

--- src/components/RadioInput.jsx
+++ src/components/RadioInput.jsx
@@ -2,24 +2,22 @@ import React from 'react';
 
 const RadioInput = ({ options, onChange, orderSelected, datatestid }) => {
   return (
-    options.map((tipo) => {
-      return (
-        <label
-          key={tipo}
-          htmlFor={tipo}
-        >
-          <input
-            data-testid={datatestid}
-            type="radio"
-            tipo={tipo}
-            value={tipo}
-            checked={orderSelected === tipo}
-            onChange={onChange}
-            id={tipo}
-          /> {tipo}
-        </label>
-      )
-    }
+    options.map((tipo) => (
+      <label
+        key={tipo}
+        htmlFor={tipo}
+      >
+        <input
+          data-testid={datatestid}
+          type="radio"
+          tipo={tipo}
+          value={tipo}
+          checked={orderSelected === tipo}
+          onChange={onChange}
+          id={tipo}
+        /> {tipo}
+      </label>
+    );
   )
   );
 };
