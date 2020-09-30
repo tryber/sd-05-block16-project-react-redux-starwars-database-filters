740bc5884be6c796cfc602946b682fa573f838af
Larissa Palombo <larissampalombo@gmail.com>
2020-09-30 19:03:29 -0300

    cc

--- src/service/api.js
+++ src/service/api.js
@@ -2,7 +2,7 @@ const URL = 'https://swapi-trybe.herokuapp.com/api/planets';
 
 const endpoint = () =>
   fetch(URL).then((response) =>
-    response.json().then((data) => (response.ok ? Promise.resolve(data) : Promise.reject(data)))
+    response.json().then((data) => (response.ok ? Promise.resolve(data) : Promise.reject(data))),
   );
 
 export default endpoint;
